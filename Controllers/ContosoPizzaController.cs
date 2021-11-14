dcf1d420093dc49f6d55ec2cde84e26a579f6516
jorgejunior618 <jorgejunior618@gmail.com>
2021-11-13 23:08:47 -0300

    Get methods added to PizzaController

--- Controllers/ContosoPizzaController.cs
+++ Controllers/ContosoPizzaController.cs
@@ -1,14 +1,29 @@
 using Microsoft.AspNetCore.Mvc;
+using ContosoPizzWebAPI.Services;
+using ContosoPizzWebAPI.Models;
 
-namespace ContosoPizzWebAPI.Controllers;
-
-[ApiController]
-[Route("[controller]")]
-public class PizzaController : ControllerBase 
+namespace ContosoPizzWebAPI.Controllers
 {
-    public PizzaController()
+    [ApiController]
+    [Route("[controller]")]
+    public class PizzaController : ControllerBase 
     {
+        public PizzaController()
+        {
+        }
 
+        [HttpGet]
+        public ActionResult<List<Pizza>> GetAll() => 
+            PizzaService.GetAll();
+        
+        [HttpGet("{id}")]
+        public ActionResult<Pizza> GetPizza(int id)
+        {
+            var pizza = PizzaService.GetPizza(id);
+
+            if(pizza is null) return NotFound();
+
+            return pizza;
+        }
     }
-    
 }
