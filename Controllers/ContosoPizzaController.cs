90de20508ca97f63ebf26d28cc91a9a63458bc5a
jorgejunior618 <jorgejunior618@gmail.com>
2021-11-13 22:57:12 -0300

    Controllers, Models and Services created

---
+++ Controllers/ContosoPizzaController.cs
@@ -0,0 +1,14 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace ContosoPizzWebAPI.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class PizzaController : ControllerBase 
+{
+    public PizzaController()
+    {
+
+    }
+    
+}
